f9c89c06f58d759136b8c6a3d0ae1bfbe1678802
varun kumar dendi <63526849+varundendi@users.noreply.github.com>
2022-04-26 19:00:13 +0000

    Initial commit

---
+++ Comma.java
@@ -0,0 +1,8 @@
+class Comma {
+  public static void main(String[] args) {
+    for(int a=1, b=4; a<=b ; a++ , b--){
+      System.out.println("the value of a = " + a) ;
+      System.out.println("the value of b = " + b);
+    }
+ }
+}
